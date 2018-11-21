f3657d91d8b0c3464f9fcd2503babe6989c8bd84
Jeffrey <sjfdlut@163.com>
2018-11-21 21:44:29 +0000

    Delete train_quickdraw_shoe.sh

--- train_quickdraw_shoe.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-export LD_LIBRARY_PATH="/home_directory/cudnn/lib64:$LD_LIBRARY_PATH"
-
-/home_directory/anaconda2/bin/python ./sketchrnn_cnn_train.py --dataset quickdraw_shoe --load_pretrain False --use_jade True
